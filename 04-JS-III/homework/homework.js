afe7d036082185a7a55e990afcbe62532e353967
Macarena Mancilla <Maca@Macarenas-MacBook.local>
2020-12-08 13:33:05 -0300

    tarea actualizada 7/12

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,52 +1,68 @@
 // No cambies los nombres de las funciones.
 
 function devolverPrimerElemento(array) {
-  // Devuelve el primer elemento de un array
-  // Tu código:
+
+return array[0]
+
 }
 
 function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
-  // Tu código:
+
+return array [array.length - 1]
+
 }
 
 function obtenerLargoDelArray(array) {
-  // Devuelve el largo de un array
-  // Tu código:
+
+return (array.length)
+
 }
 
 function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
+
+  var nuevoArray = []
+
+for (var i = 0; i< array.length; i++) {
+  
+  nuevoArray[i] = array[i] +1
+
+}
+
+return nuevoArray
+
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
-  // Añade el "elemento" al final del array
-  // y devuelve el array
-  // Tu código:
+  
+ array.push(elemento) ;
+
+ return array
+
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
-  // Añade el "elemento" al comienzo del array
-  // y devuelve el array
-  // Pista: usa el método `.unshift`
-  // Tu código:
+
+array.unshift(elemento) ;
+
+return array
+
 }
 
 function dePalabrasAFrase(palabras) {
-  // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
-  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
+ 
+  return palabras.join(' ')
 }
 
 function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
-  // Tu código:
+  
+ if (array.includes(elemento)){
+
+  return true
+
+ }else {
+
+  return false
+ }
 }
 
 function agregarNumeros(numeros) {
